80a72e5fcb9e7f68a0a52224128a4d751fc5588e
srigayathri199 <srigayathrik99@gmail.com>
2025-12-21 22:19:20 +0530

    first commit1

---
+++ src/test/java/Sample/LoginPageTest.java
@@ -0,0 +1,22 @@
+package Sample;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+
+public class LoginPageTest {
+    @Test
+    public void Lpge() throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+        Thread.sleep(3000);
+        driver.findElement(By.name("username")).sendKeys("Admin");
+        driver.findElement(By.name("password")).sendKeys("admin123");
+        driver.findElement(By.className("orangehrm-login-button")).click();
+
+    }
+
+
+}
