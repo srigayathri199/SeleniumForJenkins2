73507ef2e8297133c84674e456dd40b439ce4a82
srigayathri199 <srigayathrik99@gmail.com>
2025-12-22 22:59:40 +0530

    s_c

--- src/test/java/Sample/LoginPageTest.java
+++ src/test/java/Sample/LoginPageTest.java
@@ -3,18 +3,29 @@ package Sample;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 
 public class LoginPageTest {
+
+    @Parameters("Browser")
     @Test
-    public void Lpge() throws InterruptedException {
-        WebDriver driver = new ChromeDriver();
+    public void Lpge(String browserName) throws InterruptedException {
+
+        WebDriver driver = null;
+        if(browserName.contains("Chrome"))  driver = new ChromeDriver();
+        else if (browserName.contains("Edge")) driver = new EdgeDriver();
+
         driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+        driver.manage().window().maximize();
         Thread.sleep(3000);
         driver.findElement(By.name("username")).sendKeys("Admin");
         driver.findElement(By.name("password")).sendKeys("admin123");
         driver.findElement(By.className("orangehrm-login-button")).click();
+        Thread.sleep(2000);
+        driver.quit();
 
     }
 
